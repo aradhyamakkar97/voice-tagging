dbc3cc2be57cf9d1af64592e24051bce4ffe2fb9
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-06-02 18:29:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,437 @@
+$(document).ready(function(){
+
+$('#2').addClass('heighlight');
+$('#2').removeClass('heighlight');
+var a=document.getElementById("audio");
+
+setInterval(function(){
+
+
+// now- MANUAL WORK:- 
+
+  if (a.currentTime>89 && a.currentTime<102){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#1').addClass('heighlight');
+  $('#0').removeClass('heighlight');}
+
+  if (a.currentTime>102 && a.currentTime<112){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#2').addClass('heighlight');
+  $('#1').removeClass('heighlight');}
+
+  if (a.currentTime>112 && a.currentTime<127){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#3').addClass('heighlight');
+  $('#2').removeClass('heighlight');}
+
+  if (a.currentTime>127 && a.currentTime<138){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#4').addClass('heighlight');
+  $('#3').removeClass('heighlight');}
+
+  if (a.currentTime>138 && a.currentTime<144){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#5').addClass('heighlight');
+  $('#4').removeClass('heighlight');}
+
+  if (a.currentTime>144 && a.currentTime<148){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#6').addClass('heighlight');
+  $('#5').removeClass('heighlight');}
+
+  if (a.currentTime>148 && a.currentTime<166){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#7').addClass('heighlight');
+  $('#6').removeClass('heighlight');}
+
+  if (a.currentTime>166 && a.currentTime<176){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#8').addClass('heighlight');
+  $('#7').removeClass('heighlight');}
+
+  if (a.currentTime>176 && a.currentTime<187){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#9').addClass('heighlight');
+  $('#8').removeClass('heighlight');}
+
+  if (a.currentTime>187 && a.currentTime<198){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#10').addClass('heighlight');
+  $('#9').removeClass('heighlight');}
+
+  if (a.currentTime>198 && a.currentTime<209){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#11').addClass('heighlight');
+  $('#10').removeClass('heighlight');}
+
+  if (a.currentTime>209 && a.currentTime<214){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#12').addClass('heighlight');
+  $('#11').removeClass('heighlight');}
+
+  if (a.currentTime>214 && a.currentTime<220){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#13').addClass('heighlight');
+  $('#12').removeClass('heighlight');}
+
+  if (a.currentTime>220 && a.currentTime<274){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#14').addClass('heighlight');
+  $('#13').removeClass('heighlight');}
+
+  if (a.currentTime>274 && a.currentTime<279){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#15').addClass('heighlight');
+  $('#14').removeClass('heighlight');}
+
+  if (a.currentTime>279 && a.currentTime<286){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#16').addClass('heighlight');
+  $('#15').removeClass('heighlight');}
+
+  if (a.currentTime>286 && a.currentTime<300){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+    $('#17').addClass('heighlight');
+  $('#16').removeClass('heighlight');}
+
+  if (a.currentTime>300){
+    $('#0').removeClass('heighlight');
+    $('#1').removeClass('heighlight');
+    $('#2').removeClass('heighlight');
+    $('#3').removeClass('heighlight');
+    $('#4').removeClass('heighlight');
+    $('#5').removeClass('heighlight');
+    $('#6').removeClass('heighlight');
+    $('#7').removeClass('heighlight');
+    $('#8').removeClass('heighlight');
+    $('#9').removeClass('heighlight');
+    $('#10').removeClass('heighlight');
+    $('#11').removeClass('heighlight');
+    $('#12').removeClass('heighlight');
+    $('#13').removeClass('heighlight');
+    $('#14').removeClass('heighlight');
+    $('#15').removeClass('heighlight');
+    $('#16').removeClass('heighlight');
+    $('#17').removeClass('heighlight');
+
+  $('#17').removeClass('heighlight');}
+
+},100);
+});
+
+
+
+
+
+
+/*
+$('#0').removeClass('heighlight');
+$('#1').removeClass('heighlight');
+$('#2').removeClass('heighlight');
+$('#3').removeClass('heighlight');
+$('#4').removeClass('heighlight');
+$('#5').removeClass('heighlight');
+$('#6').removeClass('heighlight');
+$('#7').removeClass('heighlight');
+$('#8').removeClass('heighlight');
+$('#9').removeClass('heighlight');
+$('#10').removeClass('heighlight');
+$('#11').removeClass('heighlight');
+$('#12').removeClass('heighlight');
+$('#13').removeClass('heighlight');
+$('#14').removeClass('heighlight');
+$('#15').removeClass('heighlight');
+$('#16').removeClass('heighlight');
+$('#17').removeClass('heighlight');
+
+
+*/
